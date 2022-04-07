5cfba574c90dd247ba233c784ee0b81641d48148
Mikhail Malyarenko <malyarenko.md@gmail.com>
2022-04-07 18:13:08 +0300

    dev: genetic operators module added

--- src/genalg.rs
+++ src/genalg.rs
@@ -1,2 +1,3 @@
 pub mod chromo;
+pub mod operators;
 pub mod alg;
---
+++ src/genalg/operators.rs
@@ -0,0 +1,13 @@
+use crate::genalg::chromo::Chromo;
+
+pub fn select(population: &[Chromo], target_size: usize) -> Vec<Chromo> {
+    todo!()
+}
+
+pub fn mutate(chromo: &mut Chromo) {
+    todo!()
+}
+
+pub fn cross(chromo_a: &Chromo, chromo_b: &Chromo) -> (Chromo, Chromo) {
+    todo!()
+}
