d62a06a24a458178f11032514aa6f1de9ec50d59
Mikhail Malyarenko <malyarenko.md@gmail.com>
2022-04-11 21:23:37 +0300

    feat: new crossing algorithm

--- src/genalg/operators.rs
+++ src/genalg/operators.rs
@@ -1,10 +1,11 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::rc::Rc;
 
 use rand::Rng;
 
 use crate::genalg::chromo::Chromo;
 use crate::graph::VarLifetimeGraph as Graph;
+use crate::lifetime::VarId;
 
 pub fn select_ranking(population: &[Chromo], target_size: usize) -> Vec<Chromo> {
     if population.is_empty() {
@@ -77,35 +78,90 @@ pub fn mutate(chromo: &mut Chromo) {
     chromo.swap_genes(locus_a, locus_b);
 }
 
-pub fn cross(parent_a: &Chromo, parent_b: &Chromo, graph: Rc<Graph>) -> Chromo {
-    if parent_a.size() != parent_a.size() {
+pub fn cross(
+    parents: (&Chromo, &Chromo),
+    graph: Rc<Graph>,
+    eval_matrix: &HashMap<(VarId, VarId), u16>,
+) -> Vec<Chromo> {
+    if parents.0.size() != parents.1.size() {
         panic!("crossing chromosomes with different size")
     }
 
-    let mut used_id_set = HashSet::new();
+    fn inner(
+        dominant: &Chromo,
+        recessive: &Chromo,
+        cross_point: usize,
+        graph: &Graph,
+        eval_matrix: &HashMap<(VarId, VarId), u16>,
+    ) -> Vec<VarId> {
+        let dominant_gene = dominant.gene().split_at(cross_point);
+        let recessive_gene = recessive.gene().split_at(cross_point);
 
-    let child_gene = parent_a
-        .gene()
-        .iter()
-        .zip(parent_b.gene())
-        .map(|(&id_a, &id_b)| {
-            if used_id_set.contains(&id_a) {
-                used_id_set.insert(id_b);
-                id_b
-            } else if used_id_set.contains(&id_b) {
-                used_id_set.insert(id_a);
-                id_a
-            } else {
-                if graph.nodes[&id_a].deg < graph.nodes[&id_b].deg {
-                    used_id_set.insert(id_a);
-                    id_a
+        let dominant_gene_tail: HashSet<VarId> = dominant_gene.1.iter().copied().collect();
+        let recessive_gene_tail: HashSet<VarId> = recessive_gene.1.iter().copied().collect();
+
+        let mut gene_tail_diff = &dominant_gene_tail - &recessive_gene_tail;
+        let gene_tail_intersect = &dominant_gene_tail & &recessive_gene_tail;
+
+        /* Child gene with None on conflict IDs */
+        let child_gene: Vec<Option<VarId>> = dominant_gene
+            .0
+            .iter()
+            .map(|&id| Some(id))
+            .chain(recessive_gene.1.iter().map(|id| {
+                if gene_tail_intersect.contains(id) {
+                    Some(*id)
                 } else {
-                    used_id_set.insert(id_b);
-                    id_b
+                    None
                 }
-            }
-        })
-        .collect();
+            }))
+            .collect();
 
-    Chromo::new(child_gene, graph)
+        /* Resolving conflict IDs */
+        let mut prev_id = 0;
+        let child_gene: Vec<VarId> = child_gene
+            .iter()
+            .enumerate()
+            .map(|(locus, &opt_id)| {
+                if let Some(id) = opt_id {
+                    prev_id = id;
+                    id
+                } else {
+                    if locus == 0 {
+                        /* If no previous IDs - choose node ID with min degree */
+                        let &id = gene_tail_diff
+                            .iter()
+                            .min_by_key(|&id| graph.nodes[id].deg)
+                            .expect("empty graph");
+                        prev_id = id;
+                        gene_tail_diff.remove(&id);
+                        id
+                    } else {
+                        /* Select node ID with by minimal evaluation wight */
+                        let &id = gene_tail_diff
+                            .iter()
+                            .min_by_key(|&id| eval_matrix[&(prev_id, *id)])
+                            .unwrap();
+                        prev_id = id;
+                        gene_tail_diff.remove(&id);
+                        id
+                    }
+                }
+            })
+            .collect();
+
+        child_gene
+    }
+
+    let mut rng = rand::thread_rng();
+
+    let cross_point = rng.gen_range(0..parents.0.size());
+
+    let child_a_gene = inner(&parents.0, &parents.1, cross_point, &graph, eval_matrix);
+    let child_b_gene = inner(&parents.1, &parents.0, cross_point, &graph, eval_matrix);
+
+    vec![
+        Chromo::new(child_a_gene, graph.clone()),
+        Chromo::new(child_b_gene, graph),
+    ]
 }
